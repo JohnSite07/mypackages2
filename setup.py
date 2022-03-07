8b82ff426f27965550dccf49c6237a72e626c775
Luc Sita <jeen.mb@gmail.com>
2022-03-07 22:27:35 +0200

    Fisrt commit

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='mypackage2',
-    version='0.1'
-    packages=find_packages(exclude=['tests*']),
+    version='0.1',
+    packages=find_packages(exclude=['tests']),
     license='MIT',
-    description='EDSA example python package'
+    description='EDSA example python package',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
     url='https://github.com/JohnSite07/mypackages2.git',
-    author='<Jean Luc>'
+    author='<Jean Luc>',
     author_email='<jeen.mb@gmail.com>'
 )
